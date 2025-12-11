802e05fbcc5342da42e524d000eee6038d7d47fa
Jijo James <jj.jamesjijo@gmail.com>
2025-12-11 22:17:20 +0530

    revert config changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/sunseekers-kayaking-v1/',
+  base: '/',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
